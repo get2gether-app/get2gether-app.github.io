eecfaef6d9519267e87c19d14de888f714542dc3
Muhtalip Dede <muhtalipdede@gmail.com>
2023-06-20 20:18:42 +0300

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: "export",
+    basePath: "/",
+    images: {
+        unoptimized: true,
+    },
+}
 
 module.exports = nextConfig
