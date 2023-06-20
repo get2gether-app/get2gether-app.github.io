0ba62f2f0937e9125d9f4196e091c446ad30b24f
Muhtalip Dede <muhtalipdede@gmail.com>
2023-06-20 20:41:14 +0300

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Analytics from './lib/analytics'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,6 +17,25 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script 
+          src="https://www.googletagmanager.com/gtag/js?id=G-PL96RVLDWT"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', 'G-PL96RVLDWT');
+            `,
+          }}
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
---
+++ app/lib/analytics.tsx
@@ -0,0 +1,9 @@
+import Script from 'next/script';
+
+export default function Analytics() {
+  return (
+    <>
+      
+    </>
+  );
+}
